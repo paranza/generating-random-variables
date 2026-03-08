d6b11fba404f94e90894e84f11277715c554370d
paranza <an.perazza@gmail.com>
2026-03-08 21:38:38 +0100

    Refactor FWL theorem implementation with comments
    Added comments and structured code for clarity on the Frisch–Waugh–Lovell theorem application.

--- FWL_Thm.R
+++ FWL_Thm.R
@@ -1,21 +1,24 @@
-#Traditional multilinear regression
+# Code for Frisch–Waugh–Lovell (FWL) theorem on the first regressor of a multivariate linear regression
+
+# Traditional multilinear regression
 model <- with(houses, lm(price ~ lot + bdrms + fb + sty + drive + rec +
                            ffinbmnt + ghw + ca + garage + pref))
 summary(model) #beta1 estimate is 3.546
 
-#Applying FWL Thm:
+# Applying FWL Thm:
 
-#Step 1: regress X1 on X2, ..., Xk
+# Step 1: regress X1 on X2, ..., Xk
 model1 <- with(houses, lm(lot ~ bdrms + fb + sty + drive + rec +
                             ffinbmnt + ghw + ca + garage + pref))
-#summary(model1)
+# summary(model1)
 resx1 <- residuals(model1) #Save residuals 
 
-#Step 2: regress Y on X2, ..., Xk
+# Step 2: regress Y on X2, ..., Xk
 model2 <- with(houses, lm(price ~ bdrms + fb + sty + drive + rec +
                             ffinbmnt + ghw + ca + garage + pref))
-#summary(model2)
+# summary(model2)
 resy <- residuals(model2) #Save residuals 
 
 model3 <- with(houses, lm(resy ~ resx1))
+
 summary(model3) #coeff estimate is 3.546, exactly beta1
