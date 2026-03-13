35e2d83a8735814c498ee921a38ae0951568cf06
paranza <an.perazza@gmail.com>
2026-03-13 09:16:34 +0100

    Add files via upload

---
+++ ols-perfect-multicollinearity.R
@@ -0,0 +1,16 @@
+#Testing that if a regressor can be described perfectly 
+#with a linear function of another regressor, the ols 
+#gives N/A for that first regressor
+
+x = runif(1000,0,4)
+b1 = 1 
+b0 = 1
+y_true = b0 + b1*x
+eps = rnorm(1000,0,0.3)
+y_noise = y_true + eps
+
+reg1 = rbinom(1000,1,0.3)
+reg2 = 1 - reg1 #dummy var
+
+model = lm(y_noise ~ reg1 + reg2)
+summary(model) #reg2 is N/A, linearly dependant from reg1 
